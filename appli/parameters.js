e423714e466158a50f809bd6c1b42beac0a0421e
solenn-tl <solenn.tual@gmail.com>
2023-10-04 11:10:00 +0200

    Add web app

---
+++ appli/parameters.js
@@ -0,0 +1,10 @@
+/***********************************************************************
+ * Adresse du répertoire GraphDB où se trouve le graph spatio-temporel *
+ * *********************************************************************/
+
+/* Remplacer la valeur actuelle par l'adresse de votre répertoire */
+
+//var repertoireGraphDB = "https://dir.geohistoricaldata.org/sparql/"
+var repertoireGraphDB = "http://HPE2101P101:7200/repositories/soduco"
+
+var createclusters = true
