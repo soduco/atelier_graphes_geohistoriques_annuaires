359216233daff9381cd642f16984f35b4d5956e4
solenn-tl <solenn.tual@gmail.com>
2023-10-05 12:34:47 +0200

    URL update

--- parameters.js
+++ parameters.js
@@ -4,7 +4,7 @@
 
 /* Remplacer la valeur actuelle par l'adresse de votre répertoire */
 
-//var repertoireGraphDB = "https://dir.geohistoricaldata.org/sparql/"
-var repertoireGraphDB = "http://HPE2101P101:7200/repositories/soduco"
+var repertoireGraphDB = "https://dir.geohistoricaldata.org/sparql/"
+//var repertoireGraphDB = "http://HPE2101P101:7200/repositories/soduco"
 
 var createclusters = true
