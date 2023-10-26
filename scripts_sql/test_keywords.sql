a74dd0c302421065d53da9391cc21370393de2b5
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-10-26 17:56:41 +0200

    Update test_keywords.sql
    Adapted query to directories_v2 schema

--- scripts_sql/test_keywords.sql
+++ scripts_sql/test_keywords.sql
@@ -1,24 +1,26 @@
 WITH per_count AS (
-			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
-			FROM directories.elements AS e
+			SELECT e.uuid, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories_v2.entries AS e
 			ORDER BY count_ DESC
 		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
-			SELECT pc.index
+			SELECT pc.uuid
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index, p.ner_xml AS person, act.ner_xml AS activity, s.loc AS loc, s.cardinal AS cardinal,
-	t.ner_xml AS title, e.directory, e.published, lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')) AS fulladd
+	SELECT DISTINCT e.uuid, p.per AS person, act.act AS activity, s.loc AS loc, s.cardinal AS cardinal,
+	t.titre AS title, src.code_ouvrage, src.liste_annee, lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')) AS fulladd
 	FROM short_list AS l
-	INNER JOIN directories.elements AS e ON l.index = e.index
-	INNER JOIN directories.persons AS p ON e.index = p.entry_id
-	INNER JOIN directories.activities AS act ON e.index = act.entry_id
-	INNER JOIN directories.addresses AS s ON e.index = s.entry_id
-	INNER JOIN directories.titles AS t ON e.index = t.entry_id
+	INNER JOIN directories_v2.entries AS e ON l.uuid = e.uuid
+	INNER JOIN directories_v2.persons AS p ON e.uuid = p.entry_uuid
+	INNER JOIN directories_v2.activities AS act ON e.uuid = act.entry_uuid
+	INNER JOIN directories_v2.addresses AS s ON e.uuid = s.entry_uuid
+	INNER JOIN directories_v2.titles AS t ON e.uuid = t.entry_uuid
+	INNER JOIN directories_v2.sources AS src ON e.source_uuid = src.uuid
 	WHERE (
 		-- Liste des mots-clés: à adapter!
-		act.ner_xml ILIKE '%ébéniste%' OR 
-		act.ner_xml ILIKE '%ebeniste%' OR 
-		act.ner_xml ILIKE '%ébeniste%' OR 
-		act.ner_xml ILIKE '%ebéniste%' 
+		act.act ILIKE '%ébéniste%' OR 
+		act.act ILIKE '%ebeniste%' OR 
+		act.act ILIKE '%ébeniste%' OR 
+		act.act ILIKE '%ebéniste%' 
 		)
-	ORDER BY e.index, e.published ASC;
+	ORDER BY src.liste_annee ASC;
+
