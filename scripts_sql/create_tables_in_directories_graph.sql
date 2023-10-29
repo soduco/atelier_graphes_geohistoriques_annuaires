0db2030e196252d3e217340867d088008a6df39a
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-10-29 15:13:33 +0100

    Update create_tables_in_directories_graph.sql
    Update with geocoding

--- scripts_sql/create_tables_in_directories_graph.sql
+++ scripts_sql/create_tables_in_directories_graph.sql
@@ -1,32 +1,41 @@
+/* Suppression des tables de liens */
+DROP TABLE IF EXISTS directories_graph.liens;
+DROP TABLE IF EXISTS directories_graph.liens_externes;
+
+/*Création de la table directories_content*/
 DROP TABLE IF EXISTS directories_graph.directories_content;
 
 CREATE TABLE directories_graph.directories_content AS (
 	--Pour chaque entrée, compte le nombre de '<PER>' dans la chaîne XML retournée par la pipeline NER
 	WITH per_count AS (
-			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
-			FROM directories.elements AS e
+			SELECT e.uuid, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories_v2.entries AS e
 			ORDER BY count_ DESC
 		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
-			SELECT pc.index
+			SELECT pc.uuid
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index, p.ner_xml AS person, act.ner_xml AS activity, s.loc AS loc, s.cardinal AS cardinal,
-	t.ner_xml AS title, e.directory, e.published, lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')) AS fulladd
+	SELECT DISTINCT e.uuid, TRANSLATE(p.per,',.:;\-_\(\)\[\]?!$&*/','') AS person, TRANSLATE(act.act,',.:;\-_\(\)\[\]?!$&*/','') AS activity, s.loc AS loc, s.cardinal AS cardinal,
+	t.titre AS title, src.code_ouvrage AS directory, src.liste_annee AS published, TRANSLATE(lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')),',.:;_\(\)\[\]?!$&*/','') AS fulladd, s.uuid AS id_address
 	FROM short_list AS l
-	INNER JOIN directories.elements AS e ON l.index = e.index
-	INNER JOIN directories.persons AS p ON e.index = p.entry_id
-	INNER JOIN directories.activities AS act ON e.index = act.entry_id
-	INNER JOIN directories.addresses AS s ON e.index = s.entry_id
-	INNER JOIN directories.titles AS t ON e.index = t.entry_id
+	INNER JOIN directories_v2.entries AS e ON l.uuid = e.uuid
+	INNER JOIN directories_v2.persons AS p ON e.uuid = p.entry_uuid
+	INNER JOIN directories_v2.activities AS act ON e.uuid = act.entry_uuid
+	INNER JOIN directories_v2.addresses AS s ON e.uuid = s.entry_uuid
+	INNER JOIN directories_v2.titles AS t ON e.uuid = t.entry_uuid
+	INNER JOIN directories_v2.sources AS src ON e.source_uuid = src.uuid
 	WHERE (
+		-- Sélection des entrées issues des listes par noms
+        (src.liste_type ILIKE '%ListNoms%') AND
 		-- Liste des mots-clés
-		act.ner_xml ILIKE '%atlas%' OR 
-		act.ner_xml ILIKE '%cartes géograph%' OR 
-		act.ner_xml ILIKE '%lithogra%' OR 
-		(act.ner_xml ILIKE '%grav%' AND act.ner_xml ILIKE '%douce%') OR 
-		(act.ner_xml ILIKE '%grav%' AND act.ner_xml ILIKE '%méta%')
+		(act.act ILIKE '%atlas%' AND act.act ILIKE '%cart%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%géo%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%geo%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%marin%')OR
+		(act.act ILIKE '%plan%' AND act.act ILIKE '%topograph%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%topograph%')
 		)
-	ORDER BY e.index, e.published ASC);
+	ORDER BY src.liste_annee ASC);
 	
 
 
@@ -36,50 +45,57 @@ DROP TABLE IF EXISTS directories_graph.geocoding;
 CREATE TABLE directories_graph.geocoding AS (
 	--Pour chaque entrée, compte le nombre de '<PER>' dans la chaîne XML retournée par la pipeline NER
 	WITH per_count AS (
-			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
-			FROM directories.elements AS e
+			SELECT e.uuid, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories_v2.entries AS e
 			ORDER BY count_ DESC
 		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
-			SELECT pc.index
+			SELECT pc.uuid
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index as entry_id, g.index as id_address, g."precise.geo_response" AS precise_geo_response, ST_Transform(ST_GeomFromText(g."precise.geom",2154),4326) AS precise_geom
+	SELECT DISTINCT e.uuid as entry_id, s.uuid as address_id, gcd.housenumber, gcd.street, gcd.locality , gcd.source, ST_Transform(gcd.geometry,4326) AS precise_geom
 	FROM short_list AS l
-	INNER JOIN directories.elements AS e ON l.index = e.index
-	INNER JOIN directories.activities AS act ON e.index = act.entry_id
-	INNER JOIN directories.geocoding AS g ON e.index = g.entry_id
+	INNER JOIN directories_v2.entries AS e ON l.uuid = e.uuid
+	INNER JOIN directories_v2.addresses AS s ON s.entry_uuid = e.uuid
+    INNER JOIN directories_v2.geocoded_address_gazetteer AS gcd ON gcd.uuid = s.geocoded_gazetteer_ref_uuid
+    INNER JOIN directories_v2.sources AS src ON e.source_uuid = src.uuid
+	INNER JOIN directories_v2.activities AS act ON e.uuid = act.entry_uuid
 	WHERE (
-		(g."precise.geo_response" not like '')
-		AND(
+		-- Sélection des entrées issues des listes par noms
+        (src.liste_type ILIKE '%ListNoms%') AND
+		-- Sélection des géocodages non nuls
+		(gcd.street not like '') AND
 		-- Liste des mots-clés
-		act.ner_xml ILIKE '%atlas%' OR 
-		act.ner_xml ILIKE '%cartes géograph%' OR 
-		act.ner_xml ILIKE '%lithogra%' OR 
-		(act.ner_xml ILIKE '%grav%' AND act.ner_xml ILIKE '%douce%') OR 
-		(act.ner_xml ILIKE '%grav%' AND act.ner_xml ILIKE '%méta%')
-		))
-	ORDER BY e.index);
+		((act.act ILIKE '%atlas%' AND act.act ILIKE '%cart%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%géo%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%geo%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%marin%')OR
+		(act.act ILIKE '%plan%' AND act.act ILIKE '%topograph%')OR
+		(act.act ILIKE '%cart%' AND act.act ILIKE '%topograph%')
+	))
+	ORDER BY e.uuid ASC);
 
 	
 /* ********************************************************* */
 /*    Ajout d'une clé primaire à la table principale                               */
 /* ******************************************************** */
-ALTER TABLE directories_graph.directories_content ADD COLUMN entry_id serial PRIMARY KEY;
+ALTER TABLE directories_graph.directories_content ADD COLUMN entry_id uuid ;
+UPDATE directories_graph.directories_content SET entry_id = gen_random_uuid ();
+ALTER tABLE directories_graph.directories_content ADD PRIMARY KEY (entry_id);
 /* ********************************************************* */
 /*    Ajout d'une colonne pour stocker le graphe nommé des données                               */
 /* ******************************************************** */
 ALTER TABLE directories_graph.directories_content ADD COLUMN graph_name character varying (50);	
-UPDATE directories_graph.directories_content SET graph_name ='graveurs_de_cartes';
+UPDATE directories_graph.directories_content SET graph_name ='cartes_et_plans';
 ALTER TABLE directories_graph.geocoding ADD COLUMN graph_name character varying (50);	
-UPDATE directories_graph.geocoding SET graph_name ='graveurs_de_cartes';
+UPDATE directories_graph.geocoding SET graph_name ='cartes_et_plans';
 
 /* ********************************************************* */
 /*    On complète la table des graphes nommés                */
 /* ******************************************************** */
-DROP TABLE directories_graph.dataset;
+DROP TABLE IF EXISTS directories_graph.dataset;
 CREATE TABLE directories_graph.dataset(title character varying(100), issued date, graph_name character varying(100));
-ALTER TABLE directories_graph.dataset ADD COLUMN dataset_id serial PRIMARY KEY;
-INSERT INTO directories_graph.dataset VALUES ('Graveurs et imprimeurs de cartes', '2023-09-27', 'graveurs_de_cartes');
+ALTER TABLE directories_graph.dataset ADD COLUMN dataset_id uuid PRIMARY KEY;
+INSERT INTO directories_graph.dataset VALUES ('Graveurs et marchands de cartes et plans', '2023-10-29', 'cartes_et_plans', gen_random_uuid ());
 
 
 /* ********************************************************* */
@@ -90,8 +106,8 @@ DROP TABLE IF EXISTS directories_graph.liens;
 
 CREATE TABLE IF NOT EXISTS directories_graph.liens
 (
-    entry_id1 bigint,
-    entry_id2 bigint,
+    entry_id1 uuid,
+    entry_id2 uuid,
     CONSTRAINT entry_id1_fk FOREIGN KEY (entry_id1)
         REFERENCES directories_graph.directories_content (entry_id) MATCH SIMPLE
         ON UPDATE CASCADE
@@ -108,7 +124,7 @@ DROP TABLE IF EXISTS directories_graph.liens_externes;
 
 CREATE TABLE IF NOT EXISTS directories_graph.liens_externes
 (
-    entry_id bigint,
+    entry_id uuid,
     uri text COLLATE pg_catalog."default",
     CONSTRAINT entry_id_fk FOREIGN KEY (entry_id)
         REFERENCES directories_graph.directories_content (entry_id) MATCH SIMPLE
@@ -120,13 +136,14 @@ CREATE TABLE IF NOT EXISTS directories_graph.liens_externes
 /* ********************************************************* */
 /*    Ajout des index               */
 /* ******************************************************** */
-create index on directories_graph.directories_content(index);
+create index on directories_graph.directories_content(entry_id);
+create index on directories_graph.directories_content(uuid);
 create index on directories_graph.directories_content(person);
 create index on directories_graph.directories_content(activity);
 create index on directories_graph.directories_content(loc);
 create index on directories_graph.directories_content(directory);
+create index on directories_graph.directories_content(published);
 create index on directories_graph.directories_content(fulladd);
-create index on directories_graph.directories_content(entry_id);
 create index on directories_graph.directories_content(graph_name);
 
 create index on directories_graph.dataset(title);
@@ -135,9 +152,12 @@ create index on directories_graph.dataset(dataset_id);
 create index on directories_graph.dataset(graph_name);
 
 create index on directories_graph.geocoding(entry_id);
-create index on directories_graph.geocoding(id_address);
+create index on directories_graph.geocoding(address_id);
 create index on directories_graph.geocoding(graph_name);
-create index on directories_graph.geocoding(precise_geo_response);
+create index on directories_graph.geocoding(housenumber);
+create index on directories_graph.geocoding(street);
+create index on directories_graph.geocoding(locality);
+create index on directories_graph.geocoding(source);
 CREATE INDEX index_geocoding_geom
 ON directories_graph.geocoding
 USING gist (precise_geom);
@@ -147,8 +167,3 @@ create index on directories_graph.liens(entry_id2);
 
 create index on directories_graph.liens_externes(entry_id);
 create index on directories_graph.liens_externes(uri);
-
-
-
-
-
