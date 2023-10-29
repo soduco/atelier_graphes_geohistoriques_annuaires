1932a638ce9056e649741c99b58cc80976938024
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-10-29 15:14:44 +0100

    Update insert_new_graph_data.sql
    Update with geocoding

--- scripts_sql/insert_new_graph_data.sql
+++ scripts_sql/insert_new_graph_data.sql
@@ -1,58 +1,67 @@
-INSERT INTO directories_graph.directories_content 
+INSERT INTO directories_graph.directories_content (uuid, person, activity,loc, cardinal,title,directory,published, fulladd, id_address, entry_id)
 (
 	--Pour chaque entrée, compte le nombre de '<PER>' dans la chaîne XML retournée par la pipeline NER
 	WITH per_count AS (
-			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
-			FROM directories.elements AS e
+			SELECT e.uuid, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories_v2.entries AS e
 			ORDER BY count_ DESC
 		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
-			SELECT pc.index
+			SELECT pc.uuid
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index, p.ner_xml AS person, act.ner_xml AS activity, s.loc AS loc, s.cardinal AS cardinal,
-	t.ner_xml AS title, e.directory, e.published, lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')) AS fulladd
+	SELECT DISTINCT e.uuid, TRANSLATE(p.per,',.:;\-_\(\)\[\]?!$&*/','') AS person, TRANSLATE(act.act,',.:;\-_\(\)\[\]?!$&*/','') AS activity, s.loc AS loc, s.cardinal AS cardinal,
+	t.titre AS title, src.code_ouvrage AS directory, src.liste_annee AS published, TRANSLATE(lower((COALESCE(s.loc,'') || ' '::text) || COALESCE(s.cardinal,'')),',.:;_\(\)\[\]?!$&*/','') AS fulladd, s.uuid AS id_address, gen_random_uuid ()
 	FROM short_list AS l
-	INNER JOIN directories.elements AS e ON l.index = e.index
-	INNER JOIN directories.persons AS p ON e.index = p.entry_id
-	INNER JOIN directories.activities AS act ON e.index = act.entry_id
-	INNER JOIN directories.addresses AS s ON e.index = s.entry_id
-	INNER JOIN directories.titles AS t ON e.index = t.entry_id
+	INNER JOIN directories_v2.entries AS e ON l.uuid = e.uuid
+	INNER JOIN directories_v2.persons AS p ON e.uuid = p.entry_uuid
+	INNER JOIN directories_v2.activities AS act ON e.uuid = act.entry_uuid
+	INNER JOIN directories_v2.addresses AS s ON e.uuid = s.entry_uuid
+	INNER JOIN directories_v2.titles AS t ON e.uuid = t.entry_uuid
+	INNER JOIN directories_v2.sources AS src ON e.source_uuid = src.uuid
 	WHERE (
+		-- Sélection des entrées issues des listes par noms
+        (src.liste_type ILIKE '%ListNoms%') AND
 		-- Liste des mots-clés
-		act.ner_xml ILIKE '%ébéniste%' OR 
-		act.ner_xml ILIKE '%ebeniste%' OR 
-		act.ner_xml ILIKE '%ébeniste%' OR 
-		act.ner_xml ILIKE '%ebéniste%' 
-		)
-	ORDER BY e.index, e.published ASC);
+		(act.act ILIKE '%nouveauté%' 
+      OR act.act ILIKE '%nouveaute%' 
+      OR act.act ILIKE '%nouvaute%' 
+	  OR (act.act ILIKE '%march%' AND act.act ILIKE '%nouv%')
+	  OR (act.act ILIKE '%magasin%' AND act.act ILIKE '%nouv%') 
+      OR act.act ILIKE '%nouv.%' ))
+	ORDER BY src.liste_annee ASC);
 	
 INSERT INTO directories_graph.geocoding 
 (
 	--Pour chaque entrée, compte le nombre de '<PER>' dans la chaîne XML retournée par la pipeline NER
 	WITH per_count AS (
-			SELECT e.index, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
-			FROM directories.elements AS e
+			SELECT e.uuid, (length(e.ner_xml) - length(replace(e.ner_xml, '<PER>', '' ))) / length('<PER>') AS count_
+			FROM directories_v2.entries AS e
 			ORDER BY count_ DESC
 		), short_list AS ( --Ne conserve que les entrées avec 0 ou 1 PER (au delà, on aura un produit cartésien de tous les attributs)
-			SELECT pc.index
+			SELECT pc.uuid
 			FROM per_count AS pc
 			WHERE count_ <=1)
-	SELECT DISTINCT e.index as entry_id, g.index as id_address, g."precise.geo_response" AS precise_geo_response, ST_Transform(ST_GeomFromText(g."precise.geom",2154),4326) AS precise_geom
+	SELECT DISTINCT e.uuid as entry_id, s.uuid as address_id, gcd.housenumber, gcd.street, gcd.locality , gcd.source, ST_Transform(gcd.geometry,4326) AS precise_geom
 	FROM short_list AS l
-	INNER JOIN directories.elements AS e ON l.index = e.index
-	INNER JOIN directories.activities AS act ON e.index = act.entry_id
-	INNER JOIN directories.geocoding AS g ON e.index = g.entry_id
+	INNER JOIN directories_v2.entries AS e ON l.uuid = e.uuid
+	INNER JOIN directories_v2.addresses AS s ON s.entry_uuid = e.uuid
+    INNER JOIN directories_v2.geocoded_address_gazetteer AS gcd ON gcd.uuid = s.geocoded_gazetteer_ref_uuid
+    INNER JOIN directories_v2.sources AS src ON e.source_uuid = src.uuid
+	INNER JOIN directories_v2.activities AS act ON e.uuid = act.entry_uuid
 	WHERE (
-		(g."precise.geo_response" not like '')
-		AND(
+		-- Sélection des entrées issues des listes par noms
+        (src.liste_type ILIKE '%ListNoms%') AND
+		-- Sélection des géocodages non nuls
+		(gcd.street not like '') AND
 		-- Liste des mots-clés
-				act.ner_xml ILIKE '%ébéniste%' OR 
-		act.ner_xml ILIKE '%ebeniste%' OR 
-		act.ner_xml ILIKE '%ébeniste%' OR 
-		act.ner_xml ILIKE '%ebéniste%'))
-	ORDER BY e.index);
+		(act.act ILIKE '%nouveauté%' 
+      OR act.act ILIKE '%nouveaute%' 
+      OR act.act ILIKE '%nouvaute%' 
+	  OR (act.act ILIKE '%march%' AND act.act ILIKE '%nouv%')
+	  OR (act.act ILIKE '%magasin%' AND act.act ILIKE '%nouv%') 
+      OR act.act ILIKE '%nouv.%' ))
+	ORDER BY e.uuid ASC);
 	
-UPDATE directories_graph.directories_content SET graph_name ='ebenistes' WHERE graph_name ISNULL;
-UPDATE directories_graph.geocoding SET graph_name ='ebenistes' WHERE graph_name ISNULL;
-INSERT INTO directories_graph.dataset VALUES ('Ebenistes', '2023-09-27', 'ebenistes');
-	
+UPDATE directories_graph.directories_content SET graph_name ='nouveautes_test' WHERE graph_name ISNULL;
+UPDATE directories_graph.geocoding SET graph_name ='nouveautes_test' WHERE graph_name ISNULL;
+INSERT INTO directories_graph.dataset VALUES ('Magasins de nouveautés', '2023-10-29', 'nouveautes_test', gen_random_uuid ());
