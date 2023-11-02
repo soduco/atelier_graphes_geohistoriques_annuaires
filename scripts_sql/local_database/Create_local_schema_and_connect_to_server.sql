33d1b52c5bd2c8dce22f8e3f258ccea85a6636f4
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-11-02 14:40:01 +0100

    Added local type

--- scripts_sql/local_database/Create_local_schema_and_connect_to_server.sql
+++ scripts_sql/local_database/Create_local_schema_and_connect_to_server.sql
@@ -1,11 +1,18 @@
 -- Création des deux schémas locaux
-DROP SCHEMA IF EXISTS directories_local;
 DROP SCHEMA IF EXISTS directories_graph;
-CREATE SCHEMA directories_local;
+DROP SCHEMA IF EXISTS directories_v2;
+CREATE SCHEMA directories_v2;
 CREATE SCHEMA directories_graph;
+
+-- Création d'un type local pour pouvoir "importer" les données distantes
+-- DROP TYPE IF EXISTS directories_v2.element_type;
+CREATE TYPE directories_v2.element_type AS ENUM
+    ('PAGE', 'TITLE_LEVEL_1', 'TITLE_LEVEL_2', 'SECTION_LEVEL_1', 'SECTION_LEVEL_2', 'COLUMN_LEVEL_1', 'COLUMN_LEVEL_2', 'LINE', 'ENTRY');
+ALTER TYPE directories_v2.element_type
+    OWNER TO postgres;
 
 -- Connexion au serveur distant pour importer les données dans la base locale: les tables distantes sont lisibles dans les ForeignTables du schéma directories_local
 CREATE EXTENSION postgres_fdw;
 CREATE SERVER acces_soduco FOREIGN DATA WRAPPER postgres_fdw OPTIONS (host 'geohistoricaldata.org', dbname 'soduco', port '5432');
 CREATE USER MAPPING FOR postgres SERVER acces_soduco OPTIONS (user 'soduco-ro-all', password 'soduco-ro-all');
-IMPORT FOREIGN SCHEMA directories_v2 FROM SERVER acces_soduco INTO directories_local;
+IMPORT FOREIGN SCHEMA directories_v2 FROM SERVER acces_soduco INTO directories_v2;
