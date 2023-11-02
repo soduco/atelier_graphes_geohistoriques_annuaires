e4aae025f6c386a0f60de4f0b4e8b647bdfa56b2
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-11-02 19:52:09 +0100

    Update Delete_named_graph_data.sql

--- scripts_sql/soduco_database/Delete_named_graph_data.sql
+++ scripts_sql/soduco_database/Delete_named_graph_data.sql
@@ -1,6 +1,6 @@
-DELETE * FROM directories_graph.directories_content dc
+DELETE FROM directories_graph.directories_content dc
 WHERE dc.graph_name Like 'ebenistes';
-DELETE * FROM directories_graph.dataset dst
+DELETE FROM directories_graph.dataset dst
 WHERE dst.graph_name Like 'ebenistes';
-DELETE * FROM directories_graph.geocoding gcd
+DELETE FROM directories_graph.geocoding gcd
 WHERE gcd.graph_name Like 'ebenistes';
