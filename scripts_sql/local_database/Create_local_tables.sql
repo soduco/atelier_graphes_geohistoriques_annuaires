254e6d1a04458ca0a50b01aa171dd24daa2d237d
Nathalie Abadie <nathalie-f.abadie@ign.fr>
2023-11-05 20:30:39 +0100

    Update Create_local_tables.sql

--- scripts_sql/local_database/Create_local_tables.sql
+++ scripts_sql/local_database/Create_local_tables.sql
@@ -13,97 +13,91 @@ DROP TABLE IF EXISTS directories_graph.dataset;
 CREATE TABLE directories_graph.directories_content AS (
 	/* Filtrage des activités */
 	WITH filtered_activities AS (
-	SELECT * 
-	FROM activities 
-	WHERE 
-		/* ************************************************************* */
+    SELECT *
+    FROM activities
+    WHERE   /* ************************************************************* */
         /* Modifier la liste des mots-clés selon les données à extraire  */
         /* ************************************************************* */
-	    (act ILIKE '%atlas%' AND act ILIKE '%cart%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%géo%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%geo%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%marin%')OR
-		(act ILIKE '%plan%' AND act ILIKE '%topograph%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%topograph%')
+        (act ILIKE '%atlas%' AND act ILIKE '%cart%')OR
+        (act ILIKE '%cart%' AND act ILIKE '%géo%')OR
+        (act ILIKE '%cart%' AND act ILIKE '%geo%')OR
+        (act ILIKE '%cart%' AND act ILIKE '%marin%')OR
+        (act ILIKE '%plan%' AND act ILIKE '%topograph%')OR
+        (act ILIKE '%cart%' AND act ILIKE '%topograph%')
+),
+aggregated AS (
+    SELECT  entries.uuid,
+            ARRAY_AGG(per) OVER (PARTITION BY persons.entry_uuid) AS per,
+            ARRAY_AGG(titre) OVER (PARTITION BY titles.entry_uuid) AS titre,
+            act,
+            trim(concat(cardinal, ' ', loc)) AS fulladd,
+            loc,
+            cardinal,
+			addresses.uuid AS address_id,
+			addresses.geocoded_gazetteer_uuid,
+            sources.code_ouvrage,
+            sources.liste_annee,
+            sources.liste_type
+    FROM filtered_activities
+    JOIN entries
+    ON filtered_activities.entry_uuid = entries.uuid
+    AND filtered_activities.source = entries.source
+    JOIN ( SELECT * FROM sources WHERE liste_type = 'ListNoms' ) AS sources
+    ON entries.source_uuid = sources.uuid
+    LEFT JOIN persons
+    ON persons.entry_uuid = filtered_activities.entry_uuid
+    AND persons.source = filtered_activities.source
+    LEFT JOIN titles
+    ON titles.entry_uuid = filtered_activities.entry_uuid
+    AND titles.source = filtered_activities.source
+    LEFT JOIN addresses
+    ON addresses.entry_uuid = filtered_activities.entry_uuid
+    AND addresses.source = filtered_activities.source
 )
-	SELECT  entries.uuid, TRANSLATE(per,'.:;\-_\(\)\[\]?!$&*/','') AS person, TRANSLATE(act,'.:;\-_\(\)\[\]?!$&*/','') AS activity, loc, cardinal, titre AS title, sources.code_ouvrage AS directory, sources.liste_annee AS published, trim(concat(cardinal, ' ', loc)) AS fulladd, addresses.uuid as id_address 
-	FROM filtered_activities 
-	JOIN entries
-		ON filtered_activities.entry_uuid = entries.uuid
-		AND filtered_activities.source = entries.source
-	JOIN sources
-		ON entries.source_uuid = sources.uuid
-	JOIN addresses
-		ON addresses.entry_uuid = filtered_activities.entry_uuid
-		AND addresses.source = filtered_activities.source
-	LEFT JOIN LATERAL (
-		SELECT string_agg(per, ',') AS per
-		FROM persons
-		WHERE persons.entry_uuid = filtered_activities.entry_uuid
-			AND persons.source = filtered_activities.source
-			AND per IS NOT NULL
-		GROUP BY entry_uuid
-	) AS persons
-	ON True
-	LEFT JOIN LATERAL (
-		SELECT string_agg(titre, ',') AS titre
-		FROM titles
-		WHERE titles.entry_uuid = filtered_activities.entry_uuid
-			AND titles.source = filtered_activities.source
-			AND titre IS NOT NULL
-		GROUP BY entry_uuid
-	) AS titles
-	ON True
-	WHERE sources.liste_type = 'ListNoms');
+SELECT  DISTINCT ON(act, fulladd, loc, cardinal, address_id, geocoded_gazetteer_uuid, code_ouvrage, liste_annee)
+		uuid,
+        STRING_AGG(DISTINCT per, ',') AS person,
+        STRING_AGG(DISTINCT titre, ',') AS title,
+        act,
+        fulladd,
+        loc,
+        cardinal,
+		address_id,
+		geocoded_gazetteer_uuid,
+	    code_ouvrage,
+        liste_annee
+FROM
+(
+    SELECT  uuid,
+            UNNEST(per) AS per,
+            UNNEST(titre) AS titre,
+            act,
+            fulladd,
+            loc,
+            cardinal,
+			address_id,
+			geocoded_gazetteer_uuid,
+            code_ouvrage,
+            liste_annee,
+            liste_type
+    FROM aggregated
+) AS sub
+GROUP BY uuid, act, fulladd, loc, cardinal, address_id, geocoded_gazetteer_uuid, code_ouvrage, liste_annee
+);
 
 /*Création de la table geocoding*/
-CREATE TABLE directories_graph.geocoding AS (
-	/* Filtrage des activités */
-	WITH filtered_activities AS (
-	SELECT * 
-	FROM activities 
-	WHERE 
-		/* ************************************************************* */
-        /* Modifier la liste des mots-clés selon les données à extraire  */
-        /* ************************************************************* */
-	    (act ILIKE '%atlas%' AND act ILIKE '%cart%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%géo%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%geo%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%marin%')OR
-		(act ILIKE '%plan%' AND act ILIKE '%topograph%')OR
-		(act ILIKE '%cart%' AND act ILIKE '%topograph%')
-)
-	SELECT  entries.uuid as entry_id, addresses.uuid as address_id, geocoded_address_gazetteer.housenumber, geocoded_address_gazetteer.street, geocoded_address_gazetteer.locality, geocoded_address_gazetteer.source, ST_Transform(geocoded_address_gazetteer.geometry,4326) AS precise_geom
-	FROM filtered_activities 
-	JOIN entries
-		ON filtered_activities.entry_uuid = entries.uuid
-		AND filtered_activities.source = entries.source
-	JOIN sources
-		ON entries.source_uuid = sources.uuid
-	JOIN addresses
-		ON addresses.entry_uuid = filtered_activities.entry_uuid
-		AND addresses.source = filtered_activities.source
-	JOIN geocoded_address_gazetteer
-		ON addresses.geocoded_gazetteer_uuid = geocoded_address_gazetteer.uuid
-	LEFT JOIN LATERAL (
-		SELECT string_agg(per, ',') AS per
-		FROM persons
-		WHERE persons.entry_uuid = filtered_activities.entry_uuid
-			AND persons.source = filtered_activities.source
-			AND per IS NOT NULL
-		GROUP BY entry_uuid
-	) AS persons
-	ON True
-	LEFT JOIN LATERAL (
-		SELECT string_agg(titre, ',') AS titre
-		FROM titles
-		WHERE titles.entry_uuid = filtered_activities.entry_uuid
-			AND titles.source = filtered_activities.source
-			AND titre IS NOT NULL
-		GROUP BY entry_uuid
-	) AS titles
-	ON True
-	WHERE sources.liste_type = 'ListNoms');
+CREATE TABLE directories_graph.geocoding AS
+SELECT	
+	directories_content.uuid as entry_id, 
+	address_id, 
+	geocoded_address_gazetteer.housenumber, 
+	geocoded_address_gazetteer.street, 
+	geocoded_address_gazetteer.locality, 
+	geocoded_address_gazetteer.source, 
+	ST_Transform(geocoded_address_gazetteer.geometry,4326) AS precise_geom
+FROM directories_graph.directories_content
+LEFT JOIN geocoded_address_gazetteer
+ON directories_content.geocoded_gazetteer_uuid = geocoded_address_gazetteer.uuid;
 	
 -- Ajout d'une clé primaire à la table principale 
 ALTER TABLE directories_graph.directories_content ADD COLUMN entry_id uuid ;
@@ -118,9 +112,9 @@ ALTER TABLE directories_graph.dataset ADD COLUMN dataset_id uuid PRIMARY KEY;
 /* ****************************************************************************************************************************** */
 /* Modifier les propriétés du graphe nommmé selon les données extraites: identifiant, titre du jeu de données, date d'extraction  */
 /* ****************************************************************************************************************************** */
-UPDATE directories_graph.directories_content SET graph_name ='nouveautes';
-UPDATE directories_graph.geocoding SET graph_name ='nouveautes';
-INSERT INTO directories_graph.dataset VALUES ('Magasins de nouveautes', '2023-11-02', 'nouveautes', gen_random_uuid ());
+UPDATE directories_graph.directories_content SET graph_name ='cartes_et_plans';
+UPDATE directories_graph.geocoding SET graph_name ='cartes_et_plans';
+INSERT INTO directories_graph.dataset VALUES ('Graveurs et marchands de cartes et plans', '2023-11-07', 'cartes_et_plans', gen_random_uuid ());
 
 -- Ajout des tables de liens
 CREATE TABLE IF NOT EXISTS directories_graph.liens
