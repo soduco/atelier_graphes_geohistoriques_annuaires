a19314068c4d9ca05b4ed6ce92799fc44e506c23
solenn-tl <solenn.tual@gmail.com>
2023-11-07 07:32:21 +0100

    Local baselayer

--- map.js
+++ map.js
@@ -76,7 +76,8 @@ map.on(L.Draw.Event.CREATED, function (e) {
     "Jacoubet (1836)":wmsJacoubet,
     "Andriveau (1849)":wmsAndriveau,
     "Atlas municipal (1888)":wmsbhdv,
-    "Plan IGN (2022)":GeoportailFrance_plan
+    "Plan IGN (2022)":GeoportailFrance_plan,
+    "Open Data Paris (2023)":openDataParis
 }
 
 var overLayers = {
